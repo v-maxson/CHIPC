163e429eab05185568be92326b51fe508e0f2e73
Maxson <93727813+v-maxson@users.noreply.github.com>
2022-08-16 12:06:23 -0600

    feat: add utility functions

---
+++ src/chipc_utility.h
@@ -0,0 +1,9 @@
+#ifndef CHIPC_CHIPC_UTILITY_H
+#define CHIPC_CHIPC_UTILITY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+uint64_t CHIPC_GetFileSize(const char* file_path, bool* result);
+
+#endif //CHIPC_CHIPC_UTILITY_H
