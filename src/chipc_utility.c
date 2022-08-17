8cc6ad44564794fd0a91bccc0d44a7cea2ada558
Maxson <93727813+v-maxson@users.noreply.github.com>
2022-08-17 17:55:15 -0600

    feat: add disclaimer

--- src/chipc_utility.c
+++ src/chipc_utility.c
@@ -1,5 +1,7 @@
 #include "chipc_utility.h"
 
+// Disclaimer: This implementation requires a POSIX Compliant system to compile.
+// This works on most popular systems (Linux, Windows, Mac), but not all.
 #include <sys/stat.h>
 
 uint64_t CHIPC_GetFileSize(const char *file_path, bool *result) {
