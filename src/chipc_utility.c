dc493ec75d232556d4215f1d4395a9a62bca4f82
Maxson <93727813+v-maxson@users.noreply.github.com>
2022-08-17 17:46:32 -0600

    feat: reimplement file size function using stat

--- src/chipc_utility.c
+++ src/chipc_utility.c
@@ -1,29 +1,14 @@
 #include "chipc_utility.h"
 
-#ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <sys/stat.h>
 
 uint64_t CHIPC_GetFileSize(const char *file_path, bool *result) {
-    OFSTRUCT of;
-    HFILE file = OpenFile(file_path, &of, 0);
-    if (!file) {
+    struct stat file_stat;
+    if (stat(file_path, &file_stat) < 0) {
         if (result) *result = false;
         return 0;
     }
 
-    DWORD size = GetFileSize((HANDLE)file, NULL);
-    CloseHandle((HANDLE)file);
-
     if (result) *result = true;
-    return size;
+    return file_stat.st_size;
 }
-
-#else
-// TODO: CHIPC_GetFileSize Linux/POSIX implementation.
-
-uint64_t CHIPC_GetFileSize(const char *file_path, bool *result) {
-    if (result) *result = false;
-    return 0;
-}
-#endif
