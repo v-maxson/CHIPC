e600f7e3059c348b116781db0a96f10b9cf4070c
Maxson <93727813+v-maxson@users.noreply.github.com>
2022-08-17 23:21:11 -0600

    feat: implement basic cpu structure

--- src/cpu.c
+++ src/cpu.c
@@ -1,1 +1,16 @@
 #include "cpu.h"
+#include <string.h>
+
+#define CHIPC_STACK_SIZE 16
+#define CHIPC_PC_START 512
+
+CHIPC_Cpu CHIPC_CreateCpu(void) {
+    CHIPC_Cpu cpu = {0};
+
+    cpu.stack = CHIPC_CreateStack(16);
+    cpu.pc = CHIPC_PC_START;
+
+    return cpu;
+}
+
+void CHIPC_DestroyCpu(CHIPC_Cpu *cpu) {}
--- src/cpu.h
+++ src/cpu.h
@@ -1,8 +1,38 @@
 #ifndef CHIPC_CPU_H
 #define CHIPC_CPU_H
 
-//typedef struct CHIPC_Cpu {
-//
-//} CHIPC_Cpu;
+#include "stack.h"
+#include <stdint.h>
+
+#define CHIPC_MEMORY_SIZE 4096
+#define CHIPC_V_REGISTER_COUNT 16
+
+typedef struct CHIPC_Cpu {
+    uint8_t memory[CHIPC_MEMORY_SIZE];
+    uint8_t v[CHIPC_V_REGISTER_COUNT];
+    uint16_t i, pc;
+    uint8_t dt, st;
+    CHIPC_Stack stack;
+} CHIPC_Cpu;
+
+/**
+ * Creates a CHIPC_Cpu.
+ */
+CHIPC_Cpu CHIPC_CreateCpu(void);
+
+/**
+ * Destroys the provided CHIPC_Cpu.
+ * Currently unused, present for forwards compatibility.
+ * @param cpu The CHIPC_Cpu to destroy.
+ */
+void CHIPC_DestroyCpu(CHIPC_Cpu* cpu);
+
+/**
+ * Begin cycling the CHIPC_Cpu at the given clock speed.
+ * @param cpu The CHIPC_Cpu cycle.
+ * @param rom_path The path to the ROM to execute.
+ * @param clock_speed The speed at which to cycle (in Hz).
+ */
+void CHIPC_RunCpu(CHIPC_Cpu* cpu, const char* rom_path, float clock_speed);
 
 #endif //CHIPC_CPU_H
