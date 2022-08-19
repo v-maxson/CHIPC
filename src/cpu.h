24c6a2922ed7caa6b3f6a13d6daecbfce9996412
Maxson <93727813+v-maxson@users.noreply.github.com>
2022-08-19 00:17:15 -0600

    fix: use integer over float

--- src/cpu.h
+++ src/cpu.h
@@ -6,9 +6,17 @@
 
 #define CHIPC_MEMORY_SIZE 4096
 #define CHIPC_V_REGISTER_COUNT 16
+#define CHIPC_STACK_SIZE 16
+#define CHIPC_PC_START 512
+#define CHIPC_WINDOW_WIDTH 64
+#define CHIPC_WINDOW_HEIGHT 32
+#define CHIPC_WINDOW_DIMENSIONS 64 * 32
+#define CHIPC_PIXEL_ON  0xFFFFFFFF
+#define CHIPC_PIXEL_OFF 0x00000000
 
 typedef struct CHIPC_Cpu {
     uint8_t memory[CHIPC_MEMORY_SIZE];
+    uint32_t display_memory[CHIPC_WINDOW_DIMENSIONS];
     uint8_t v[CHIPC_V_REGISTER_COUNT];
     uint16_t i, pc;
     uint8_t dt, st;
@@ -32,6 +40,6 @@ void CHIPC_DestroyCpu(CHIPC_Cpu* cpu);
  * @param rom_path The path to the ROM to execute.
  * @param clock_speed The speed at which to cycle (in Hz).
  */
-void CHIPC_RunCpu(CHIPC_Cpu* cpu, const char* rom_path, float clock_speed);
+void CHIPC_RunCpu(CHIPC_Cpu* cpu, const char* rom_path, uint64_t clock_speed);
 
 #endif //CHIPC_CPU_H
